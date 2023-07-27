8fae5cec27e90b466ac6b49d948f53ca03684a17
Krystynka86 <71220344+Krystynka86@users.noreply.github.com>
2023-07-27 21:24:25 +0200

    Initial Commit

---
+++ Devote/DevoteApp.swift
@@ -0,0 +1,20 @@
+//
+//  DevoteApp.swift
+//  Devote
+//
+//  Created by Cristina on 2023-07-27.
+//
+
+import SwiftUI
+
+@main
+struct DevoteApp: App {
+    let persistenceController = PersistenceController.shared
+
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+                .environment(\.managedObjectContext, persistenceController.container.viewContext)
+        }
+    }
+}
