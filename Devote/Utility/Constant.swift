00c6bada243583281e78ac1255f3f32b2c82f6fe
Krystynka86 <71220344+Krystynka86@users.noreply.github.com>
2023-07-28 11:59:26 +0200

    add more items to Core Data

--- Devote/Utility/Constant.swift
+++ Devote/Utility/Constant.swift
@@ -8,10 +8,14 @@
 import SwiftUI
 
 // MARK: - FORMATTER
-private let itemFormatter: DateFormatter = {
+    let itemFormatter: DateFormatter = {
     let formatter = DateFormatter()
     formatter.dateStyle = .short
     formatter.timeStyle = .medium
     return formatter
 }()
+
+// MARK: - UI
+
+// MARK: - UX
 
