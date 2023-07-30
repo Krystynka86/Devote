1e3743a82ded0faf9dee9a22ae422e1b6c2b7d1d
Krystynka86 <71220344+Krystynka86@users.noreply.github.com>
2023-07-30 21:39:28 +0200

    create a custom check box - toggle

---
+++ Devote/Style/CheckBoxStyle.swift
@@ -0,0 +1,31 @@
+//
+//  CheckBoxStyle.swift
+//  Devote
+//
+//  Created by Cristina on 2023-07-30.
+//
+
+import SwiftUI
+
+struct CheckBoxStyle: ToggleStyle {
+    func makeBody(configuration: Configuration) -> some View {
+        return HStack {
+            Image(systemName: configuration.isOn ? "checkmark.circle.fill" : "circle")
+                .foregroundColor(configuration.isOn ? .pink : .primary)
+                .font(.system(size: 30, weight: .semibold, design: .rounded))
+                .onTapGesture {
+                    configuration.isOn.toggle()
+                }
+            configuration.label
+        } //: HSTACK
+    }
+}
+
+struct CheckBoxStyle_Previews: PreviewProvider {
+    static var previews: some View {
+        Toggle("Placeholder label", isOn: .constant(true))
+            .toggleStyle(CheckBoxStyle())
+            .padding()
+            .previewLayout(.sizeThatFits)
+    }
+}
