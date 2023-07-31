3a8cd642265b5e653cb129a5ba4e8591d2d45115
Krystynka86 <71220344+Krystynka86@users.noreply.github.com>
2023-07-31 11:57:05 +0200

    add static widget

---
+++ DevoteWidget/DevoteWidgetBundle.swift
@@ -0,0 +1,17 @@
+//
+//  DevoteWidgetBundle.swift
+//  DevoteWidget
+//
+//  Created by Cristina on 2023-07-30.
+//
+
+import WidgetKit
+import SwiftUI
+
+@main
+struct DevoteWidgetBundle: WidgetBundle {
+    var body: some Widget {
+        DevoteWidget()
+        DevoteWidgetLiveActivity()
+    }
+}
